1115ab72d404b4a07a033a5c36b54e9672ccc62b
Knyazev Egor <knyazevegor01@mail.ru>
2022-03-01 22:44:15 +0300

    Fix bugs in core

--- 03_MergeRequirements/bullscows/__init__.py
+++ 03_MergeRequirements/bullscows/__init__.py
@@ -4,15 +4,17 @@ import random
 
 def bullscows(guess: str, secret: str) -> (int, int):
     bulls = td.hamming.similarity(guess, secret)
-    cows = td.bag.similarity(quess, secret)
+    cows = td.bag.similarity(guess, secret)
     return (bulls, cows)
 
 
 def gameplay(ask: callable, inform: callable, words: list[str]) -> int:
     secret = random.choice(words)
-    b = 0
+    guess = ''
+    b = 1
     cntr = 0
-    while (b != len(guess := ask("Введите слово: ", words))):
+    while b != len(secret) or b != len(guess):
+        guess = ask("Введите слово: ", words)
         cntr += 1
         b, c = bullscows(guess, secret)
         inform("Быки: {}, Коровы: {}", b, c)
